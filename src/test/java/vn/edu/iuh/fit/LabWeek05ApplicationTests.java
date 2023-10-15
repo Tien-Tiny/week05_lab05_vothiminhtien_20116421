e4517e8a8cc7e6512707a532087010309ec2e4c3
Tien-Tiny <vothiminhtien0202@gmail.com>
2023-10-15 08:09:31 +0700

    first commit

---
+++ src/test/java/vn/edu/iuh/fit/LabWeek05ApplicationTests.java
@@ -0,0 +1,30 @@
+package vn.edu.iuh.fit;
+
+import com.neovisionaries.i18n.CountryCode;
+import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import vn.edu.iuh.fit.backend.models.Address;
+import vn.edu.iuh.fit.backend.repositories.AddressRepository;
+
+@SpringBootTest
+class LabWeek05ApplicationTests {
+	@Autowired
+	private AddressRepository repository;
+
+	@PostConstruct
+	void add_test() {
+		Address add=new Address("11","NVQ","sg","5345643", CountryCode.AF);
+		add.setId(1L);
+		repository.save(add);
+
+	}
+	@Test
+	void test_01(){
+		Address add =repository.findById(1L).get();
+		Assertions.assertEquals(add.getNumber(),"11");
+	}
+
+}
